025451a6a95f2749a41b913c7b2797588b7f0d23
candy3171434 <candy31714343171434@gmail.com>
2023-09-19 13:09:21 +0800

    test import

---
+++ demo-1/src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Todo list 相關api")
+@RestController
+public class TodoController {
+
+	
+	@GetMapping(value="/api",produces = {"text/plain;charset=UTF-8"})
+	@ApiOperation(value="取得所有代辦事項列表", httpMethod = "GET")
+	@ApiResponses({ @ApiResponse(code = 401, message = "沒有權限"), @ApiResponse(code = 404, message = "找不到路徑") })
+	public String test( @RequestParam("name") String name) {
+		String res="你的name is:"+name;
+		return res;
+	}
+}
