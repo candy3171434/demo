7046f91a5856cb8bed4df22c3867e5342fa82911
candy3171434 <candy31714343171434@gmail.com>
2023-09-19 11:12:41 +0800

    test3

--- demo-1/src/main/java/com/example/demo/controller/TodoController.java
+++ demo-1/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,46 +18,15 @@ import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Todo list 相關api")
 @RestController
-public class TodoController {
+public class TodoController
+{
 
-	
-	@GetMapping(value="/api",produces = {"text/plain;charset=UTF-8"})
-	@ApiOperation(value="取得所有代辦事項列表", httpMethod = "GET")
+	@GetMapping(value = "/api", produces = { "text/plain;charset=UTF-8" })
+	@ApiOperation(value = "取得所有代辦事項列表", httpMethod = "GET")
 	@ApiResponses({ @ApiResponse(code = 401, message = "沒有權限"), @ApiResponse(code = 404, message = "找不到路徑") })
-	public String test( @RequestParam("name") String name) {
-		String res="你的name is:"+name;
+	public String test(@RequestParam("name") String name)
+	{
+		String res = "你的name is:" + name;
 		return res;
 	}
 }
-=======
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-
-@Api(tags = "Todo list 相關api")
-@RestController
-public class TodoController {
-
-	
-	@GetMapping(value="/api",produces = {"text/plain;charset=UTF-8"})
-	@ApiOperation(value="取得所有代辦事項列表", httpMethod = "GET")
-	@ApiResponses({ @ApiResponse(code = 401, message = "沒有權限"), @ApiResponse(code = 404, message = "找不到路徑") })
-	public String test( @RequestParam("name") String name) {
-		String res="你的name is:"+name;
-		return res;
-	}
-}
->>>>>>> branch 'master' of https://github.com/candy3171434/demo.git
