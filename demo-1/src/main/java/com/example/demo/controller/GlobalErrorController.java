30d30a53214c3c640e381f7296b8ee9b90e78c11
candy3171434 <candy31714343171434@gmail.com>
2023-09-20 10:36:57 +0800

    TEST:orElseThrow

--- demo-1/src/main/java/com/example/demo/controller/GlobalErrorController.java
+++ demo-1/src/main/java/com/example/demo/controller/GlobalErrorController.java
@@ -20,7 +20,7 @@ public class GlobalErrorController
 	public ResponseEntity<BaseResult> handleException(RuntimeException e)
 	{
 		BaseResult result = new BaseResult(ResponseCode.INTERNAL_ERROR);
-		System.out.println("ddddd" + result);
+		System.out.println("錯誤訊息在這:"+e.getMessage());
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
 	}
 }
--- demo-1/src/main/java/com/example/demo/controller/TodoController.java
+++ demo-1/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.management.RuntimeErrorException;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.enumFile.ResponseCode;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -20,18 +26,18 @@ import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Todo list 相關api")
 @RestController
-public class TodoController {
-	
-	//,produces = {"text/plain;charset=UTF-8"}
-	//无法将@ExceptionHandler返回的数据转换@RequestMapping.produces所要求的格式。
-	@GetMapping(value="/api")
-	@ApiOperation(value="取得所有代辦事項列表", httpMethod = "GET")
+public class TodoController
+{
+
+	// ,produces = {"text/plain;charset=UTF-8"}
+	// 无法将@ExceptionHandler返回的数据转换@RequestMapping.produces所要求的格式。
+	@GetMapping(value = "/api")
+	@ApiOperation(value = "取得所有代辦事項列表", httpMethod = "GET")
 	@ApiResponses({ @ApiResponse(code = 401, message = "沒有權限"), @ApiResponse(code = 404, message = "找不到路徑") })
-	public ResponseEntity<String> test( @RequestParam("name") String name) {
-		String res="你的name is:"+name;
-		if(true)
-			throw new RuntimeException();
-		System.out.println("THIS IS ONE");
+	public ResponseEntity<String> test(@RequestParam("name") String name)
+	{
+		String res = "你的name is:" + name;
 		return ResponseEntity.ok(res);
 	}
+	
 }
