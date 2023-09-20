a672b10483b15a9298c5d3a9f943106f938f9912
candy3171434 <candy31714343171434@gmail.com>
2023-09-20 15:51:13 +0800

    解決-无法将@ExceptionHandler返回的数据转换@RequestMapping.produces所要求的格式

--- demo-1/src/main/java/com/example/demo/controller/TodoController.java
+++ demo-1/src/main/java/com/example/demo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.Optional;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +17,13 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 public class TodoController
 {
-
-	// ,produces = {"text/plain;charset=UTF-8"}
-	// 无法将@ExceptionHandler返回的数据转换@RequestMapping.produces所要求的格式。
-	@GetMapping(value = "/api")
+	@GetMapping(value = "/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ApiOperation(value = "取得所有代辦事項列表", httpMethod = "GET")
 	@ApiResponses({ @ApiResponse(code = 401, message = "沒有權限"), @ApiResponse(code = 404, message = "找不到路徑") })
 	public ResponseEntity<String> test(@RequestParam("name") String name)
 	{
 		String result = "你的name is:" + name;
-
+		String result1 = null;
 		Optional.ofNullable(result).orElseThrow(() -> new RuntimeException("Data not found for name: " + name));
 		return ResponseEntity.ok(result);
 	}
